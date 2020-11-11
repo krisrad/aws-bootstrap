2b19d0a2d17f4b85add30901acf85f74041cbcde
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-11-11 15:58:44 +0530

    removing debugging steps

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -4,11 +4,10 @@ cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
 REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
-export AWS_REGION=us-east-1
-REGION=us-east-1
 
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+#export INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
 export STACK_NAME=`aws --region $REGION ec2 describe-tags \
