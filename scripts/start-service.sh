78b8ea9c4643ec13127c43aa3dce47dce459a78b
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-11-11 15:29:51 +0530

    debug region value in start_service

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -4,6 +4,8 @@ cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
 REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+export AWS_REGION=us-east-1
+REGION=us-east-1
 
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
