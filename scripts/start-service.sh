1c466d515f135d2f3bfd84fb89b7cca87fadbcf3
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-11-11 13:47:56 +0530

    correcting url

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -3,7 +3,7 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
-REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/document | jq .region -r`
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
 
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instanceid`
